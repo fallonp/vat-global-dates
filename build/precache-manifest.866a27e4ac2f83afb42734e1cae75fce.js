31009d93036b5cb6429b4259938130775a2405ae
fallonp <paul@enquotemarketing.com>
2020-03-17 11:56:10 +0000

    build

---
+++ build/precache-manifest.866a27e4ac2f83afb42734e1cae75fce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44b06dbdfb0b641b56bb0a1eeea1185a",
+    "url": "/vat-global-dates/index.html"
+  },
+  {
+    "revision": "67937572f8964f27bd57",
+    "url": "/vat-global-dates/static/css/2.540afdc6.chunk.css"
+  },
+  {
+    "revision": "460ef3b7ee7ef64cb2a1",
+    "url": "/vat-global-dates/static/css/main.f86f3ab7.chunk.css"
+  },
+  {
+    "revision": "67937572f8964f27bd57",
+    "url": "/vat-global-dates/static/js/2.f0925af3.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/vat-global-dates/static/js/2.f0925af3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "460ef3b7ee7ef64cb2a1",
+    "url": "/vat-global-dates/static/js/main.6a91b49e.chunk.js"
+  },
+  {
+    "revision": "449b715bf70f50f23066",
+    "url": "/vat-global-dates/static/js/runtime-main.7eaf788a.js"
+  }
+]);
